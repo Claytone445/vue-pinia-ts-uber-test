29f29e53a14928eda580122865f8a86c7bbcaca3
Claytone 445 <brukva90@gmail.com>
2023-02-17 22:55:13 +0300

    feature add: clear project, movie card, logo,

---
+++ vue-pinia-ts/src/components/StartPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="mt-4">
+    <v-img class="mx-auto" max-width="200px" src="src/assets/tv-movie.svg">
+    </v-img>
+
+    <v-card class="mx-auto" max-width="600">
+      <v-text-field class="ma-4" :rules="rules"></v-text-field>
+      <v-img
+        class="align-end text-white"
+        height="200"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        cover
+      >
+        <v-card-title>Movie:</v-card-title>
+      </v-img>
+
+      <v-card-subtitle class="pt-4"> Number 10 </v-card-subtitle>
+
+      <v-card-text>
+        <div>Whitehaven Beach</div>
+
+        <div>Whitsunday Island, Whitsunday Islands</div>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn color="orange"> Watch </v-btn>
+
+        <v-btn color="orange"> unwatched </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "start-page",
+};
+</script>
+
+<style scoped></style>
--- vue-pinia-ts/src/stores/counter.ts
+++ vue-pinia-ts/src/stores/counter.ts
@@ -2,11 +2,11 @@ import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
 export const useCounterStore = defineStore("counter", () => {
-  const count = ref(0);
-  const doubleCount = computed(() => count.value * 2);
+  const count = ref(0); //енто стейт - реактивный стейт на композиции
+  const doubleCount = computed(() => count.value * 2); //енто геттер если шо на композиции
   function increment() {
     count.value++;
-  }
+  } //а енто тепереча так акшоны выглядят на композиции значится, пнятненько, от но чо
 
-  return { count, doubleCount, increment };
+  return { count, doubleCount, increment }; //енто на композиции все что юзается в компоненте надобно вертать
 });
