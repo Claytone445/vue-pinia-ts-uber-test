46aa452df9f1ab51c5ee50c4a3d9e6e638207cee
Claytone 445 <brukva90@gmail.com>
2023-02-20 22:49:02 +0300

    feature add: added saving watched movies into  localStorage.

--- vue-pinia-ts/src/stores/SearchStore.ts
+++ vue-pinia-ts/src/stores/SearchStore.ts
@@ -5,28 +5,6 @@ import { ref } from "vue";
 const url =
   "https:api.themoviedb.org/3/search/movie?api_key=25aaa15cb506af838b8a23910e9d8dd1&query=";
 
-/*export const useSearchStore = defineStore("searchStore", {
-  state: () => ({
-    movies: [],
-  }),
-  actions: {
-    async getMovies(search) {
-      const res = await fetch(`${url}${search}`);
-      const data = await res.json();
-      this.movies = data.results;
-      const movieStore = useMovieStore();
-      movieStore.activeTab === 1
-        ? (movieStore.activeTab = 2)
-        : (movieStore.activeTab = 1);
-    },
-    addToUserMovies(object) {
-      const movieStore = useMovieStore();
-      movieStore.movies.push({ ...object, isWatched: false });
-      movieStore.activeTab = 1;
-    },
-  },
-});*/
-
 export const useSearchStore = defineStore("searchStore", () => {
   const movies = ref([]);
 
